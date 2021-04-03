5efa786126e5416254df9132d76b8137de34356d
toxxic94 <txc@kaeng.de>
2021-04-03 19:48:44 +0200

    Create enpass.sh

---
+++ enpass.sh
@@ -0,0 +1,6 @@
+#Enpass install
+echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enpass.list
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
+
+apt update
+apt install enpass -y
